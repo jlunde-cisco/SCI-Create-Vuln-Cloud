db2eb88f8a95aadf61852d3be15c54670b00e52a
jlunde-cisco <75453543+jlunde-cisco@users.noreply.github.com>
2022-02-09 12:10:11 -0600

    finally built it

---
+++ main.tf
@@ -0,0 +1,113 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg_1" {
+  name     = "TerraformResourceGroup"
+  location = "westus2"
+
+  tags = {
+    environment = "Terraform Cloud Insights Infra"
+  }
+}
+
+resource "azurerm_virtual_network" "_10" {
+  name                = "virtual_network_10"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg_1.location
+  resource_group_name = azurerm_resource_group.rg_1.name
+}
+
+resource "azurerm_subnet" "_10-0-2" {
+  name                 = "subnet_10_0_2"
+  resource_group_name  = azurerm_resource_group.rg_1.name
+  virtual_network_name = azurerm_virtual_network._10.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "vm_public_ip"
+  resource_group_name = azurerm_resource_group.rg_1.name
+  location            = azurerm_resource_group.rg_1.location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "interface_freedom" {
+  name                = "interface_freedom"
+  location            = azurerm_resource_group.rg_1.location
+  resource_group_name = azurerm_resource_group.rg_1.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet._10-0-2.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_network_security_group" "freedom_nsg" {
+  name                = "freedom_nsg"
+  location            = azurerm_resource_group.rg_1.location
+  resource_group_name = azurerm_resource_group.rg_1.name
+
+  security_rule {
+    name                       = "freedom_allow_all"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "association" {
+  network_interface_id      = azurerm_network_interface.interface_freedom.id
+  network_security_group_id = azurerm_network_security_group.freedom_nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "freedom_linux_machine" {
+  name                = "freedom-linux-machine"
+  resource_group_name = azurerm_resource_group.rg_1.name
+  location            = azurerm_resource_group.rg_1.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.interface_freedom.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS" 
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
+  output "public_ip" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
